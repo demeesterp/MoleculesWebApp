8e3dc8ffe6f0f2a24507287d6c34d46d3217c97a
demeesterp <demeesterp@outlook.com>
2023-07-16 10:56:57 +0200

    reorganize folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { CalcOrderModule } from './CalcOrder/calc-order.module';
+import { CalcOrderModule } from './calc-order/calc-order.module';
 
 @NgModule({
   declarations: [
--- src/app/calc-order/calc-order.module.ts
+++ src/app/calc-order/calc-order.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CalcOrderComponent } from './Component/calc-order.component';
+import { CalcOrderComponent } from './components/calc-order.component';
 
 @NgModule({
   declarations: [CalcOrderComponent],

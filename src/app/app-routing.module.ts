de95cb6e4f701b57dd6ee33d1f2e560d450aa8c4
demeesterp <demeesterp@outlook.com>
2023-07-17 13:18:06 +0200

    additional changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalcOrderComponent } from './calc-order/components/calc-order.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'calc-orders', component: CalcOrderComponent },
+  { path: '',   redirectTo: '/calc-orders', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CalcOrderModule } from './calc-order/calc-order.module';
+import { AppHeaderComponent } from './app-header/components/app-header/app-header.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    CalcOrderModule
+    CalcOrderModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
